d3724ec17e3c129ad3310e7ccbe5962be4181a06
Jason Ingram <13699472+ExchMaster@users.noreply.github.com>
2020-09-02 08:50:13 -0500

    TLS1.2 Update, Asset Location, and Updated URL for Gov Cloud

--- aad-hybrid-lab/DSC/adDSC/adDSCConfiguration.ps1
+++ aad-hybrid-lab/DSC/adDSC/adDSCConfiguration.ps1
@@ -125,7 +125,8 @@ configuration DomainController
         {
             SetScript  = {
 				# Install AAD Tools
-					mkdir c:\temp -ErrorAction Ignore
+                    mkdir c:\temp -ErrorAction Ignore
+                    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 					Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 
 					Install-Module -Name MSOnline -Force
@@ -134,7 +135,7 @@ configuration DomainController
 
 					#Install-Module -Name AzureADPreview -AllowClobber -Force
 
-					#Install-Module -Name AzureRM –AllowClobber -Force
+					#Install-Module -Name AzureRM ï¿½AllowClobber -Force
 
                 }
 
